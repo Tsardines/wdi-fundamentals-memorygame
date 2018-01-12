b0d4b32e17c647590a847cd84339267238c24c4a
Haley <hjsulcer@gmail.com>
2018-01-12 18:27:13 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 
+// the array
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
+cardsInPlay;
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]){
+	alert('You found a match!');
+} else {
+	alert('Sorry try again.');
+}
+
 
 console.log("User flipped " + cardOne);
  
 console.log("User flipped " + cardTwo);
+
+
+
+
+// var cardOne[0]=1;
+
+// cardOne and cardTwo are getting pushed to cardsInPlay with jsbin
+
+// cardsInPlay.push("cardTwo");
+// cardsinPlay;
+ 
+
+// 
+// } if else { ("cardOne" === "cardTwo");
