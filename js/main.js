53e0df098f81715d1e243f33ed1be8cec3d1146e
Tsardines <hjsulcer@gmail.com>
2018-01-16 17:04:24 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,37 +1,70 @@
 
-// the array
+/* 
+   Leave the arrays below outside the flipCard function
+   because we want to be sure that the arrays have global scope
+*/
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-cardsInPlay;
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]){
-	alert('You found a match!');
-} else {
-	alert('Sorry try again.');
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
+		} else {
+			alert('Sorry, try again.');
+	}	
 }
 
 
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[0]);
+if (cardsInPlay.length === 2) {
+		
+checkForMatch();
+	}
+}
+	 
+
+	flipCard(0);
+	flipCard(2);
+
+	
+
+
+
+
+	
+
+
+	
+
+	
+
+
+
+
+
+
+
+/*
+
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
 console.log("User flipped " + cardOne);
- 
+
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
 console.log("User flipped " + cardTwo);
 
 
+var cardOne[0]=1;
 
+cardOne and cardTwo are getting pushed to cardsInPlay with jsbin
 
-// var cardOne[0]=1;
-
-// cardOne and cardTwo are getting pushed to cardsInPlay with jsbin
-
-// cardsInPlay.push("cardTwo");
-// cardsinPlay;
+cardsInPlay.push("cardTwo");
+cardsinPlay;
  
+} if else { ("cardOne" === "cardTwo");
 
-// 
-// } if else { ("cardOne" === "cardTwo");
+*/
