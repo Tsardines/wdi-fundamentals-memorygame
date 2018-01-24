5fed4845c54980c1638b925fcbe86d5c18d68f8f
Tsardines <hjsulcer@gmail.com>
2018-01-24 17:00:44 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,50 +1,64 @@
 
-/* 
-   Leave the arrays below outside the flipCard function
-   because we want to be sure that the arrays have global scope
-*/
 
-var cards = ["queen", "queen", "king", "king"];
+
+var cards = [
+
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png',
+},
+
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png',
+},
+
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png',
+},
+
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png',
+}
+
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert('You found a match!');
-		} else {
-			alert('Sorry, try again.');
+	if (cardsInPlay.length === 2) 
+
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert('You found a match!');
+			} else {
+				alert('Sorry, try again.');
 	}	
-}
+};
 
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[0]);
-if (cardsInPlay.length === 2) {
-		
-checkForMatch();
-	}
+	console.log("User flipped " + cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
 }
-	 
-
-	flipCard(0);
-	flipCard(2);
-
-	
 
 
 
+flipCard(0);
+flipCard(2); 
+
+checkForMatch();
 
 	
-
-
-	
-
-	
-
-
-
-
-
 
 
 /*
